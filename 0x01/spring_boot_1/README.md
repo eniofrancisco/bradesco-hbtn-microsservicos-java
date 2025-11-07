7cd4c67bd5b08b4e84576cd5bbc13d854b4eb6fc
eniofrancisco <enio.francisco@gmail.com>
2025-11-07 01:28:20 +0000

    Spring Boot

---
+++ 0x01/spring_boot_1/README.md
@@ -0,0 +1,15 @@
+# Projeto: Demo - Microsserviço com Spring Boot
+
+Este projeto demonstra a criação de um microsserviço simples usando Spring Boot.
+
+## 📦 Estrutura
+bradesco-hbtn-microsservicos-java/ └── 0x01/ └── spring_boot_1/ ├── demo/ │   └── src/ │       └── main/ │           └── java/ │               └── com/ │                   └── example/ │                       └── demo/ │                           ├── DemoApplication.java │                           └── MessageResource.java ├── simpleMessageWelcome_result.txt └── login_result.txt
+
+
+## 🚀 Como executar
+
+1. Certifique-se de ter o Java e o Maven instalados.
+2. Execute o projeto com:
+
+'''bash
+mvn spring-boot:run
---
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/messages")
+public class MessageResource {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+}
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/messages")
+public class MessageResource {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+}
